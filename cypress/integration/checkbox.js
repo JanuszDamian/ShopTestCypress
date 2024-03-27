0480a2318859d534e92260b058820355b87616bf
Test1 <d.dobronski@gmail.com>
2024-03-27 14:50:15 +0100

    upgrade checkbox, add checkDressesSize

--- cypress/integration/checkbox.js
+++ cypress/integration/checkbox.js
@@ -3,10 +3,16 @@
 import HomePage from "../support/page-object/homePage"
 
 describe("E2E - checkbox", () => {
-    it("CheckboxTest - automationpractice", () => {
+    it("CheckboxTest - tShirtSize", () => {
         cy.visit("/")
         HomePage.checkboxSizeTshirt();
-        HomePage.checkboxAssert();
+        HomePage.checkboxAssertM();
+    })
+
+    it("CheckboxTest - dressesSize", () => {
+        cy.visit("http://www.automationpractice.pl/index.php?id_category=10&controller=category")
+        HomePage.checkboxSizeDressesAll();
+        HomePage.checkboxAssertAllSize();
     })
 
 })
--- cypress/support/page-object/homePage.js
+++ cypress/support/page-object/homePage.js
@@ -12,11 +12,23 @@ class HomePage {
     }
 
     get tShirtLink() {
-        return cy.get('a[title="T-shirts"]').eq(1)
+        return cy.get('a[title="T-shirts"]').eq(1);
     }
 
     get checkSizeM() {
-        return cy.get("#layered_id_attribute_group_2")
+        return cy.get("#layered_id_attribute_group_2");
+    }
+
+    get dressesSizeAll() {
+        return cy.get("#ul_layered_id_attribute_group_1")
+    }
+
+    get checkSizeS() {
+        return cy.get('#layered_id_attribute_group_1')
+    }
+
+    get checkSizeL() {
+        return cy.get('#layered_id_attribute_group_3')
     }
 
     enterPhrase(text, delayValue) {
@@ -36,12 +48,29 @@ class HomePage {
         this.checkSizeM.click();
     }
 
-    checkboxAssert() {
+    checkboxAssertM() {
         this.checkSizeM.invoke("prop", "checked").then(checkedValue => {
             cy.log(checkedValue)
         })
-
     }
+
+    checkboxSizeDressesAll() {
+        this.dressesSizeAll.find("input").check();
+    }
+
+    checkboxAssertAllSize() {
+        this.checkSizeS.invoke("prop", "checked").then(checkedSValue => {
+            cy.log(checkedSValue)
+        })
+        this.checkSizeM.invoke("prop", "checked").then(checkedMValue => {
+            cy.log(checkedMValue)
+        })
+        this.checkSizeL.invoke("prop", "checked").then(checkedLValue => {
+            cy.log(checkedLValue)
+        })
+    }
+
+
 
 }
 
